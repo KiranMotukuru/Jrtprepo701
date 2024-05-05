3b2c06bb9008c5845432aaa60df0daaf47774993
kiran <129833162+KiranMotukuru@users.noreply.github.com>
2024-05-05 12:53:17 +0530

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -3,14 +3,8 @@ public class Test2 {
 	public static void main(String[] args) {
 	
 	
-I am in the Line 6	...............> Local
-<<<<<<< HEAD
-I am in the Line 7	...............> Local
+I am in the Line 6	...............> Github
 
-
-=======
-I am in the Line 7	...............> GitHub
->>>>>>> e708da3742cc497dec9e0e6ffd985f0ea68727f9
 
 
 		
