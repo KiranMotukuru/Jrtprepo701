b23f86a8fcf40ce7c9439656e94b095ff9b7e593
kiran <kiranmotukuru@gmail.com>
2024-05-05 12:34:38 +0530

    6 Line Added

--- Test2.java
+++ Test2.java
@@ -3,18 +3,7 @@ public class Test2 {
 	public static void main(String[] args) {
 	
 	
-I am in the Line 6	
-I am in the Line 7	
+I am in the Line 6	...............> Local
+		
 
-	
-I am in the Line 8
-
-		
-		
-   
-		
-		
-	}
-
-}
 
